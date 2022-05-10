cc8cee0402977d9d107a778e5718d221a1903347
Andres Castillo <ansa2000@hotmail.es>
2022-05-10 17:08:40 -0500

    Request & Body Validation

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 
 // Define Routes
