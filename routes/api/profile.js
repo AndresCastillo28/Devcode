f46bcfee15deedbebe8b4e7c802a90f5aa206a37
Andres Castillo <ansa2000@hotmail.es>
2022-05-18 11:26:08 -0500

    Add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -146,4 +146,83 @@ router.get('/user/:user_id', async(req, res) => {
     }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+
+router.delete('/', auth, async(req, res) => {
+    try {
+        // @todo - remove users posts
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });    
+        }
+    }
+});
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+
+router.put('/experience', [auth,
+    [
+    check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+        check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+        check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+ ],
+ async(req, res) => {
+    const errors = validationResult(req);   
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+        title, 
+        company, 
+        location, 
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.experience.unshift(newExp);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+ }
+);
+
 module.exports = router;
